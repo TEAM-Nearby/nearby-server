c96366825f4e7d173dbb64fb8799093d4a79c923
kimhoeyun <clnvrd@suwon.ac.kr>
2026-07-01 00:26:01 +0900

    23 refactor: Spring Modulith 모듈 공개 경계 정리

---
+++ common/src/main/java/com/sopt/nearby/common/exception/package-info.java
@@ -0,0 +1,3 @@
+// 공통 예외 계약을 다른 애플리케이션 모듈에 공개하는 패키지
+@org.springframework.modulith.NamedInterface("exception")
+package com.sopt.nearby.common.exception;
---
+++ common/src/main/java/com/sopt/nearby/common/port/package-info.java
@@ -0,0 +1,3 @@
+// 공통 포트 계약을 다른 애플리케이션 모듈에 공개하는 패키지
+@org.springframework.modulith.NamedInterface("port")
+package com.sopt.nearby.common.port;
