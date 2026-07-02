096a97b6fe07c8b1f8d8b59b448ab908c742d8b7
guswnsj0112@naver.com <guswnsj0112@naver.com>
2026-07-02 17:45:41 +0900

    chore: 주석 위치 수정

--- common/src/main/java/com/sopt/nearby/common/exception/NotFoundException.java
+++ common/src/main/java/com/sopt/nearby/common/exception/NotFoundException.java
@@ -1,6 +1,7 @@
+// 리소스를 찾지 못한 비즈니스 예외를 표현하는 공통 예외
 package com.sopt.nearby.common.exception;
 
-// 리소스를 찾지 못한 비즈니스 예외를 표현하는 공통 예외
+
 public class NotFoundException extends BusinessException {
 
     public NotFoundException(final ErrorCode errorCode) {
