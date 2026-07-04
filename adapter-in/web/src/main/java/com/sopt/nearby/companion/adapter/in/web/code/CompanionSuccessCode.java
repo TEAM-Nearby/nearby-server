64f8cefd0644451524a13a778db5af6f8c73bead
guswnsj0112@naver.com <guswnsj0112@naver.com>
2026-07-04 19:57:57 +0900

    37 feat: 매칭된 동행 목록 조회 성공 코드 및 Swagger 명세 추가

--- adapter-in/web/src/main/java/com/sopt/nearby/companion/adapter/in/web/code/CompanionSuccessCode.java
+++ adapter-in/web/src/main/java/com/sopt/nearby/companion/adapter/in/web/code/CompanionSuccessCode.java
@@ -4,7 +4,8 @@ package com.sopt.nearby.companion.adapter.in.web.code;
 import com.sopt.nearby.shared.adapter.in.web.response.SuccessCode;
 
 public enum CompanionSuccessCode implements SuccessCode {
-    READ_COMPANION_MATCH_PREVIEW("매칭된 동행 미리보기 정보를 조회했어요.");
+    READ_COMPANION_MATCH_PREVIEW("매칭된 동행 미리보기 정보를 조회했어요."),
+    READ_COMPANION_MATCHES("매칭된 동행 목록을 조회했어요.");
 
     private final String message;
 
